2e14ce26e3650e610fe63b27577ab872cc3f5e87
João Vitor <joaovirorconceicaodesoza21@gmail.com>
2026-04-12 01:52:50 -0300

    feat: adicionado refresh de token automático

--- frontend/src/api/apiConnect.ts
+++ frontend/src/api/apiConnect.ts
@@ -4,3 +4,22 @@ export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
+
+api.interceptors.response.use(
+    (response) => response, 
+    async (error) => {
+        const originalRequest = error.config
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true
+
+            try {
+                await api.post('/auth/token-refresh/')
+                return api(originalRequest)
+            } catch {
+                window.location.href = '/login'
+            }
+        }
+        return Promise.reject(error)
+    }
+)
