5bff7d7654faaf0dac8ac1c2a6a470a63d17fa68
João Vitor <joaovirorconceicaodesoza21@gmail.com>
2026-04-29 14:43:06 -0300

    fix: corrigido settings para admin

--- backend/WhyleFinance/settings/base.py
+++ backend/WhyleFinance/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
